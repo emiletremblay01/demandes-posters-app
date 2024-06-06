992dd261412fefa910395a2555b2010cc2796c67
Emile Tremblay <76021392+emiletremblay01@users.noreply.github.com>
2024-06-06 19:56:19 -0400

    fix: update badge-poster component

--- components/badge-poster.tsx
+++ components/badge-poster.tsx
@@ -1,1 +1,35 @@
+"use client";
+import type { Poster } from "@prisma/client";
+import { XIcon } from "lucide-react";
+import { Button } from "@/components/ui/button";
+import { useTransition } from "react";
+import { deletePoster } from "@/actions/poster";
+import { toast } from "sonner";
+export function BadgePoster(poster: Poster) {
+  const [isPending, startTransition] = useTransition();
 
+  const handleDelete = () => {
+    startTransition(() => {
+      deletePoster(poster.id).then((result) => {
+        if (result.error) {
+          toast(result.error);
+          return;
+        }
+        toast(result.success);
+      });
+    });
+  };
+  return (
+    <div className="flex px-3 py-1 gap-2 items-center border w-fit rounded-full">
+      <div>{poster.name}</div>
+      <Button
+        variant="ghost"
+        className="p-0 h-fit"
+        disabled={isPending}
+        onClick={handleDelete}
+      >
+        <XIcon className="size-4 translate-y-px" />
+      </Button>
+    </div>
+  );
+}
