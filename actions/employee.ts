6188c83ed67b630e5ba9450054de14b3a3000df4
Emile Tremblay <76021392+emiletremblay01@users.noreply.github.com>
2024-06-06 17:16:24 -0400

    fix: improve return statement in addEmploye action

--- actions/employee.ts
+++ actions/employee.ts
@@ -5,22 +5,27 @@ import { employeeSchema } from "@/schemas";
 import prismadb from "@/lib/prismadb";
 import { revalidatePath } from "next/cache";
 export const addEmployee = async (data: z.infer<typeof employeeSchema>) => {
-  const validatedFields = employeeSchema.safeParse(data);
-  if (!validatedFields.success) {
-    return { error: "Invalid Fields!" };
+  try {
+    const validatedFields = employeeSchema.safeParse(data);
+    if (!validatedFields.success) {
+      return { error: "Invalid Fields!" };
+    }
+  
+    const { name } = validatedFields.data;
+  
+    const result = await prismadb.employee.create({
+      data: {
+        name,
+        role: "EQUIPIER",
+      },
+    });
+  
+    revalidatePath("/settings");
+    return { success: `${result.name} successfully added to database!` };
+  } catch (error) {
+    return { error: "Erreur: " + error.message };
   }
-
-  const { name } = validatedFields.data;
-
-  const result = await prismadb.employee.create({
-    data: {
-      name,
-      role: "EQUIPIER",
-    },
-  });
-
-  revalidatePath("/settings");
-  return result;
+  
 };
 
 export const deleteEmployee = async (id: string) => {
