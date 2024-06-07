b1925ce2b712465f8ca12118061df84528bb791d
Emile Tremblay <76021392+emiletremblay01@users.noreply.github.com>
2024-06-06 20:11:06 -0400

    feat: updated settings page ui

--- app/settings/page.tsx
+++ app/settings/page.tsx
@@ -9,25 +9,29 @@ export default async function Settings() {
   const employees = await getAllEmployees();
   const posters = await getAllPosters();
   return (
-    <div className="flex flex-col p-2 pt-16 gap-2">
-      <h1 className="font-bold text-xl">Ajouter un employé</h1>
-      <div className="border rounded-md max-w-sm p-2">
-        <AddEmployeeForm />
-      </div>
-      <div className="flex flex-wrap gap-2 max-w-sm border rounded-md p-2">
-        {employees.map((employee) => (
-          <BadgeEmployee {...employee} key={employee.id} />
-        ))}
+    <div className="flex p-2 pt-16 gap-2">
+      <div className="w-full max-w-sm">
+        <h1 className="font-bold text-xl">Ajouter un employé</h1>
+        <div className="border rounded-md max-w-sm p-2">
+          <AddEmployeeForm />
+        </div>
+        <div className="flex flex-wrap gap-2 max-w-sm border rounded-md p-2">
+          {employees.map((employee) => (
+            <BadgeEmployee {...employee} key={employee.id} />
+          ))}
+        </div>
       </div>
 
-      <h1 className="font-bold text-xl">Ajouter un poster</h1>
-      <div className="border rounded-md max-w-sm p-2">
-        <AddPosterForm />
-      </div>
-      <div className="flex flex-wrap gap-2 max-w-sm border rounded-md p-2">
-        {posters.map((poster) => (
-          <BadgePoster {...poster} key={poster.id} />
-        ))}
+      <div className="w-full max-w-sm border-l">
+        <h1 className="font-bold text-xl">Ajouter un poster</h1>
+        <div className="border rounded-md max-w-sm p-2">
+          <AddPosterForm />
+        </div>
+        <div className="flex flex-wrap gap-2 max-w-sm border rounded-md p-2">
+          {posters.map((poster) => (
+            <BadgePoster {...poster} key={poster.id} />
+          ))}
+        </div>
       </div>
     </div>
   );
