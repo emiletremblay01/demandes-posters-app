c8c446a0766057cb0fa1d4832ac9cf9974385001
Emile Tremblay <emiletremblay01@gmail.com>
2024-06-09 12:48:21 -0400

    fix: added suspense wrapper

--- app/page.tsx
+++ app/page.tsx
@@ -11,38 +11,44 @@ import {
 import { getAllEmployees } from "@/data/employee";
 import { getAllPosters } from "@/data/poster";
 import { getAllPosterRequests } from "@/data/poster-request";
+import { Suspense } from "react";
 
 export default async function Home() {
   const employees = await getAllEmployees();
   const posters = await getAllPosters();
   const posterRequests = await getAllPosterRequests();
   return (
-    <div className="container flex flex-col items-center pt-20">
-      <FormAddPosterRequest {...{ employees, posters }} />
-      <Table className="mx-auto mt-20 w-full max-w-sm">
-        <TableHeader>
-          <TableRow>
-            <TableHead>Équipier</TableHead>
-            <TableHead>Poster</TableHead>
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {posterRequests.map((request) => (
-            <TableRow key={request.id}>
-              <TableCell>
-                {
-                  employees.find(
-                    (employee) => employee.id === request.employeeId,
-                  )?.name
-                }
-              </TableCell>
-              <TableCell>
-                {posters.find((poster) => poster.id === request.posterId)?.name}
-              </TableCell>
+    <Suspense>
+      <div className="container flex flex-col items-center pt-20">
+        <FormAddPosterRequest {...{ employees, posters }} />
+        <Table className="mx-auto mt-20 w-full max-w-sm">
+          <TableHeader>
+            <TableRow>
+              <TableHead>Équipier</TableHead>
+              <TableHead>Poster</TableHead>
             </TableRow>
-          ))}
-        </TableBody>
-      </Table>
-    </div>
+          </TableHeader>
+          <TableBody>
+            {posterRequests.map((request) => (
+              <TableRow key={request.id}>
+                <TableCell>
+                  {
+                    employees.find(
+                      (employee) => employee.id === request.employeeId,
+                    )?.name
+                  }
+                </TableCell>
+                <TableCell>
+                  {
+                    posters.find((poster) => poster.id === request.posterId)
+                      ?.name
+                  }
+                </TableCell>
+              </TableRow>
+            ))}
+          </TableBody>
+        </Table>
+      </div>
+    </Suspense>
   );
 }
